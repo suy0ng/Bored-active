5107c0b94859c6f23d93ab3ef8827d3009f1d316
윤수용 <yunsuyong739@naver.com>
2023-08-21 18:02:46 +0900

    Result component 완성

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Participant } from '../../types/Participant';
+import React from "react";
+import { Participant } from "../../types/Participant";
+import { BoredActivity } from "../../types/apiData/Bored";
+import * as S from "./style";
 
-interface props extends Participant{}
+interface props extends Participant , BoredActivity {}
 
-const Result : React.FC<props> = ({participant}) => {
-    return (
-        <>
-            
-        </>
-    );
+const Result: React.FC<props> = ({ participant , activity  }) => {
+
+
+  return (
+    <>
+      <S.Main>
+        <S.ResultTitle>{participant}명의 인원이 할만한 활동은 ?</S.ResultTitle>
+        <S.Result>{activity}</S.Result>
+        <S.SmallWarningText>* 해석은 알아서 하셔야합니다.</S.SmallWarningText>
+      </S.Main>
+    </>
+  );
 };
 
 export default Result;
---
+++ src/components/Result/style.ts
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+export const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f9f9f9;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ResultTitle = styled.h3``;
+export const Result = styled.h4``;
+export const SmallWarningText = styled.div`
+  font-size: 5px;
+  color: gray;
+`;
